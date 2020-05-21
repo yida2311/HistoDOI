1912d68c2602c39b5fc49b39cd580fce8396c75a
yida2311 <ldayi96@163.com>
2020-05-21 21:34:18 +0800

    fix

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,4 +1,8 @@
-from dataset.dataset_cls import OralDatasetCls
+from dataset.dataset_cls import OralDatasetCls, OralSlideCls
+from dataset.dataset_seg import OralDatasetSeg
 from dataset.transformer_cls import TransformerCls, TransformerClsVal, TransformerClsTTA
+from dataset.transformer_seg import TransformerSeg, TransformerSegVal
 
-__all__ = ['OralDatasetCls', 'TransformerCls', 'TransformerClsVal', 'TransformerClsTTA']
+__all__ = ['OralDatasetCls', 'OralSlideCls', 'OralDatasetSeg',
+    'TransformerCls', 'TransformerClsVal', 'TransformerClsTTA', 'TransformerSeg', 'TransformerSegVal']
+
