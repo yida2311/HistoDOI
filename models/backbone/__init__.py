aff7cb8ba55a31edaa382f38372ffafd3e0dc3dd
yida2311 <yida2311@foxmail.com>
2020-06-11 13:28:48 +0000

    add senet

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -2,7 +2,11 @@ from .EfficientNet.EfficientNet_backbone import EfficientNetB0
 from .EfficientNet.EfficientNet_model import EfficientNet, efficientnet_b0, efficientnet_b2
 from .ResNet.resnet_dilated import resnet_dilated_18, resnet_dilated_34, resnet_dilated_50, resnet_dilated_101, resnet_dilated_152, resnet5_dilated_18
 from .ResNet.resnet_model import resnet18, resnet34, resnet50, resnet101, resnext50_32x4d, resnext101_32x8d
+from .ResNet.senet_model import seresnext26_32x4d, seresnext50_32x4d, seresnext101_32x4d
 
 
-__all__ = ['EfficientNetB0', 'EfficientNet', 'efficientnet_b0', 'efficientnet_b2', 'resnet_dilated_18', 'resnet_dilated_34', 'resnet_dilated_50', 
-    'resnet_dilated_101', 'resnet_dilated_152', 'resnet5_dilated_18', 'resnet18', 'resnet34' 'resnet50', 'resnet101', 'resnext50_32x4d', 'resnext101_32x8d']
+__all__ = ['EfficientNetB0', 'EfficientNet', 'efficientnet_b0', 'efficientnet_b2', 
+        'resnet_dilated_18', 'resnet_dilated_34', 'resnet_dilated_50', 
+        'resnet_dilated_101', 'resnet_dilated_152', 'resnet5_dilated_18', 
+        'resnet18', 'resnet34' 'resnet50', 'resnet101', 'resnext50_32x4d', 'resnext101_32x8d',
+        'seresnext26_32x4d', 'seresnext50_32x4d', 'seresnext101_32x4d']
