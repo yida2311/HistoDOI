f78229a6e89391d1e4a0ab68633e92a4088f86dc
yida2311 <yida2311@foxmail.com>
2020-06-12 13:08:57 +0000

    senet

--- models/backbone/ResNet/senet_model.py
+++ models/backbone/ResNet/senet_model.py
@@ -18,7 +18,7 @@ import torch.utils.model_zoo as model_zoo
 
 def _cfg(url='', **kwargs):
     return {
-        'url': url, 'num_classes': 3, 'input_size': (3, 224, 224), 'pool_size': (7, 7),
+        'url': url, 'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': (7, 7),
         'crop_pct': 0.875, 'interpolation': 'bilinear',
         'mean': (0.485, 0.456, 0.406), 'std': (0.229, 0.224, 0.225),
         'first_conv': 'layer0.conv1', 'classifier': 'last_linear',
