3437f5c82281c4e488849f0425395d244de263d1
yida2311 <ldayi96@163.com>
2020-06-17 21:18:28 +0800

    meta generator

--- dataset/get_boundary_patch.py
+++ dataset/get_boundary_patch.py
@@ -3,6 +3,7 @@ import shutil
 from PIL import Image
 import numpy as np 
 import sys
+import pandas as pd 
 sys.path.append("../")
 from utils.data import boundary_patch_parser
 
@@ -48,15 +49,33 @@ def boundary_patch_getter_all(mask_dir, src_dir, src_mask_dir, save_dir, save_ma
     
     print('Num of patches for seg: {}'.format(num))
 
-
+def boundary_patch_meta(src_meta_path, dst_meta_path, data_dir):
+    src_meta = pd.read_csv(src_csv_path)
+    dst_meta = pd.DataFrame(columns=src_meta.columns)
+    for slide in os.listdir(data_dir):
+        print(slide)
+        for patch in os.listdir(os.path.join(data_dir, slide)):
+            index = src_meta[src_meta['image_id']==patch].index
+            # print(index)
+            dst_meta = dst_meta.append(src_meta.iloc[index])
+            # print(dst_meta)
+    print(dst_meta)
+    dst_meta.to_csv(dst_meta_path)
 
 
 if __name__ == '__main__':
-    mask_dir = '/media/ldy/e5a10f4e-18fd-4656-80d8-055bc4078655/OSCC-Tile/slide/mask/val_512_mask_5x'
-    src_dir = '/media/ldy/e5a10f4e-18fd-4656-80d8-055bc4078655/OSCC-Tile/seg_5x/val_512'
-    src_mask_dir = '/media/ldy/e5a10f4e-18fd-4656-80d8-055bc4078655/OSCC-Tile/seg_5x/val_mask_512'
-    save_dir = '/media/ldy/e5a10f4e-18fd-4656-80d8-055bc4078655/OSCC-Tile/seg_5x/val_bd_512'
-    save_mask_dir = '/media/ldy/e5a10f4e-18fd-4656-80d8-055bc4078655/OSCC-Tile/seg_5x/val_bd_mask_512'
+    size = 224
+    pattern = 'val'
+
+    mask_dir = '/media/ldy/e5a10f4e-18fd-4656-80d8-055bc4078655/OSCC-Tile/slide/mask/' + pattern + '_' + str(size) + '_mask_5x'
+    root = '/media/ldy/e5a10f4e-18fd-4656-80d8-055bc4078655/OSCC-Tile/5x_' + str(size) + '/'
+    src_dir = root + pattern + '_' + str(size)
+    src_mask_dir = root + pattern + '_mask_' + str(size)
+    save_dir = root + pattern + '_bd_' + str(size)
+    save_mask_dir = root + pattern + '_bd_mask_' + str(size)
+    src_csv_path  = root + pattern + '_' + str(size) + '.csv'
+    dst_csv_path  = root + pattern + '_bd_' + str(size) + '.csv'
 
     boundary_patch_getter_all(mask_dir, src_dir, src_mask_dir, save_dir, save_mask_dir)
+    boundary_patch_meta(src_csv_path, dst_csv_path, save_dir)
 
