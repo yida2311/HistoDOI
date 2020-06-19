c27688b0df662f1a514b60fbcfba0ba3b9c17533
yida2311 <ldayi96@163.com>
2020-06-19 20:58:38 +0800

    segmentation_models_pytorch

---
+++ models/segmentation_models_pytorch/seg_generator.py
@@ -0,0 +1,16 @@
+import Unet 
+
+
+unet_cfg = {
+    'encoder_depth': 5,
+    'decoder_use_batchnorm': True,
+    'decoder_channels': (512, 256, 128, 64, 32),
+    'decoder_attention_type': 'scse',
+    'in_channels': 3,
+}
+
+
+def generate_unet(num_classes, encoder_name='resnet34'):
+    return Unet(classes=num_classes, encoder_name=encoder_name, **unet_cfg)
+
+
