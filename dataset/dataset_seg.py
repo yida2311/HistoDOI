bbf3c98d33b305490fea340892a53e8cc6b1f05d
yida2311 <ldayi96@163.com>
2020-08-11 17:05:38 +0800

    dataset seg

--- dataset/dataset_seg.py
+++ dataset/dataset_seg.py
@@ -66,53 +66,51 @@ class OralDatasetSeg(Dataset):
 class OralSlideSeg(Dataset):
     """OralSlide for segmentation"""
     def __init__(self,
+                slide_list,
                 img_dir, 
-                mask_dir,
                 slide_file,
-                label=True,
+                slide_mask_dir=None,
+                label=False,
                 transform=None):
+        """
+        Params:
+            slide_list: list of slides name
+            img_dir: image directory
+            slide_file: json file, slide meta file, which include size, tiles, step
+            mask_dir: mask directory
+            label: if True, used for train/val; if False, used for test
+            transform: image preprocess
+        """
         super(OralSlideSeg, self).__init__()
+        self.slides = slide_list
         self.img_dir = img_dir
         self.label = label 
         if self.label:
-            self.mask_dir = mask_dir
+            self.slide_mask_dir = mask_dir
         self.transform = transform
 
-        self.slides = sorted(os.listdir(self.img_dir))
         with open(slide_file, 'r') as f:
             cnt = json.load(f)
         self.info = cnt  # {"slide": {"size":[h, w], "tiles": [x, y], "step":[step_x, step_y]}}
 
-        self.slide = ""
-        self.slide_mask = None
+        self.slide = "" # current slide
         self.slide_size = None
         self.slide_step = None
-        self.slide_template = None
         self.samples = []
 
     def get_slide_mask_from_index(self, index):
+        """Generate  slide mask based on index from patch mask
+            ---no use for test 
+        """
         slide = self.slides[index]
-        slide_dir = os.path.join(self.img_dir, slide)
-        samples = os.listdir(slide_dir)
-
-        size = self.info[slide]['size']
-        step = self.info[slide]['step']
-        slide_size = tuple(size)
-        slide_mask = np.zeros(slide_size, dtype='uint8')
-
-        for c in samples:
-            ver, col = self._parse_patch_name(c)
-            mask_path = os.path.join(os.path.join(self.mask_dir, self.slide), c)
-            mask = cv2_mask_loader(mask_path)
-            h, w = mask.shape
-            x = math.floor(ver*step[0]); y = math.floor(col*step[1])
-            slide_mask[x:x+h, y:y+w] = mask
+        slide_mask_dir = os.path.join(self.slide_mask_dir, slide)
+        slide_mask = cv2_mask_loader(slide_mask_dir)
         
         return slide_mask
 
     def get_patches_from_index(self, index):
+        """Collect slide info and patches based on index"""
         self.slide = self.slides[index]
-        
         slide_dir = os.path.join(self.img_dir, self.slide)
         self.samples = os.listdir(slide_dir)
 
@@ -120,8 +118,6 @@ class OralSlideSeg(Dataset):
         step = self.info[self.slide]['step']
         self.slide_size = tuple(size)
         self.slide_step = step
-        self.slide_mask = np.zeros(self.slide_size, dtype='uint8')
-        self.slide_template = np.zeros(self.slide_size, dtype='uint8')
     
     def __getitem__(self, index):
         patch = self.samples[index]
@@ -139,7 +135,6 @@ class OralSlideSeg(Dataset):
         
         ver, col = self._parse_patch_name(patch)
         sample['coord'] = (ver, col)
-
         return sample
     
     def __len__(self):
@@ -149,7 +144,6 @@ class OralSlideSeg(Dataset):
         sp = patch.split('_')
         ver = int(sp[2])
         col = int(sp[3])
-        
         return ver, col 
 
 
@@ -157,9 +151,9 @@ def collate(batch):
     batch_dict = {}
     for key in batch[0].keys():
         batch_dict[key] = [b[key] for b in batch]
-    
     batch_dict['image'] = torch.stack(batch_dict['image'], dim=0)
-    batch_dict['mask'] = torch.stack(batch_dict['mask'], dim=0)
+    if 'mask' in batch_dict.keys():
+        batch_dict['mask'] = torch.stack(batch_dict['mask'], dim=0)
 
     return batch_dict
 
