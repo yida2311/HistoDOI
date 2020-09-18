b6c55500b122af6c84e6781a7a91455cb7a0f634
yida2311 <ldayi96@163.com>
2020-09-18 22:19:20 +0800

    encoders

--- dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-from dataset.dataset_cls import OralDatasetCls, OralSlideCls
-from dataset.dataset_seg import OralDatasetSeg
-from dataset.transformer_cls import TransformerCls, TransformerClsVal, TransformerClsTTA
-from dataset.transformer_seg import TransformerSeg, TransformerSegVal
-
-__all__ = ['OralDatasetCls', 'OralSlideCls', 'OralDatasetSeg',
-    'TransformerCls', 'TransformerClsVal', 'TransformerClsTTA', 'TransformerSeg', 'TransformerSegVal']
-
--- models/segmentor/__init__.py
+++
@@ -1,5 +0,0 @@
-from .SemanticFlow.global_local_ensemble import FPN_GL, FPN_G, FPN_L, get_fpn_global, get_fpn_local
-from .SemanticFlow.fpn_semantic_flow import FPN_SF
-
-__all__ = ['FPN_SF', 'FPN_GL', 'FPN_G', 'FPN_L', 'get_fpn_global', 'get_fpn_local']
-
